14ac1919113941d56761519eda2f0d5f4d3cfc13
lziegle <71145305+lziegle@users.noreply.github.com>
2021-05-18 11:36:33 -0400

    Update tix-sidearm-fbseason.js

--- tix-sidearm-fbseason.js
+++ tix-sidearm-fbseason.js
@@ -15,77 +15,77 @@ $(document).ready(function(){
 			//Top Navs
 			$("#fbSeasonTixHomeTop").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonTixHomeTop");
-			};
+			)};
 			
 			$("#fbSeasonDigFAQTop").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonDigFAQTop");
-			};
+			)};
 			
 			$("#fbSeasonPartialTop").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonPartialTop");
-			};
+			)};
 			
 			$("#fbSeasonSingleTop").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonSingleTop");
-			};
+			)};
 			
 			$("#fbSeasonGroupTop").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonGroupTop");
-			};
+			)};
 			
 			$("#fbSeasonAwayTop").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonAwayTop");
-			};
+			)};
 			
 			$("#fbSeasonParkingTop").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonParkingTop");
-			};
+			)};
 			
 			$("#fbSeasonPromosTop").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonPromosTop");
-			};
+			)};
 			
 			$("#fbSeasonPremiumTop").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonPremiumTop");
-			};
+			)};
 			
 			//Bottom navs
 			
 			$("#fbSeasonTixHomeBtm").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonTixHomeBtm");
-			};
+			)};
 			
 			$("#fbSeasonDigFAQBtm").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonDigFAQBtm");
-			};
+			)};
 			
 			$("#fbSeasonPartialBtm").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonPartialBtm");
-			};
+			)};
 			
 			$("#fbSeasonSingleBtm").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonSingleBtm");
-			};
+			)};
 			
 			$("#fbSeasonGroupBtm").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonGroupBtm");
-			};
+			)};
 			
 			$("#fbSeasonAwayBtm").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonAwayBtm");
-			};
+			)};
 			
 			$("#fbSeasonParkingBtm").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonParkingBtm");
-			};
+			)};
 			
 			$("#fbSeasonPromosBtm").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonPromosBtm");
-			};
+			)};
 			
 			$("#fbSeasonPremiumBtm").click(function(){
 				tracker.send("event", "nav", "click", "fbSeasonPremiumBtm");
-			};
+			)};
 		}
 	}
   
