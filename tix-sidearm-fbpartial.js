02f8c7043adc8298869fd81cc34418fb2212f1e3
lziegle <71145305+lziegle@users.noreply.github.com>
2021-05-25 10:21:19 -0400

    Update tix-sidearm-fbpartial.js

--- tix-sidearm-fbpartial.js
+++ tix-sidearm-fbpartial.js
@@ -3,80 +3,12 @@ $(document).ready(function(){
 		tracker = ga.getAll()[0];
 		if (tracker) {		
 			//Top Navs
-			$("#fbPartialTixHomeTop").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialTixHomeTop");
-			});
-			
-			$("#fbPartialDigFAQTop").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialDigFAQTop");
-			});
-			
-			$("#fbPartialSeasonTop").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialSeasonTop");
-			});
-			
-			$("#fbPartialSingleTop").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialSingleTop");
-			});
-			
-			$("#fbPartialGroupTop").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialGroupTop");
-			});
-			
-			$("#fbPartialAwayTop").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialAwayTop");
-			});
-			
-			$("#fbPartialParkingTop").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialParkingTop");
-			});
-			
-			$("#fbPartialPromosTop").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialPromosTop");
-			});
-			
-			$("#fbPartialPremiumTop").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialPremiumTop");
-			});
-			
-			//Bottom navs
-			
-			$("#fbPartialTixHomeBtm").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialTixHomeBtm");
-			});
-			
-			$("#fbPartialDigFAQBtm").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialDigFAQBtm");
-			});
-			
-			$("#fbPartialSeasonBtm").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialSeasonBtm");
-			});
-			
-			$("#fbPartialSingleBtm").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialSingleBtm");
-			});
-			
-			$("#fbPartialGroupBtm").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialGroupBtm");
-			});
-			
-			$("#fbPartialAwayBtm").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialAwayBtm");
-			});
-			
-			$("#fbPartialParkingBtm").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialParkingBtm");
-			});
-			
-			$("#fbPartialPromosBtm").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialPromosBtm");
-			});
-			
-			$("#fbPartialPremiumBtm").click(function(){
-				tracker.send("event", "nav", "click", "fbPartialPremiumBtm");
+			$(".tixnav").on("click", function(event){
+				event.stopPropogation();
+				event.stopImmediatePropogation();
+				var evLabel = $(this).attr("id");
+				tracker.send("event", "nav", "click", evLabel);
 			});
 		}
 	}
-  
 });
