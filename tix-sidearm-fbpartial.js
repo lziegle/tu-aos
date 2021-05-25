441507679acb6a2cb7cbd29a028628af7d4e1e77
lziegle <71145305+lziegle@users.noreply.github.com>
2021-05-25 10:34:07 -0400

    Update tix-sidearm-fbpartial.js

--- tix-sidearm-fbpartial.js
+++ tix-sidearm-fbpartial.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
+	var lz_test = "added";
 	if ("ga" in window) {
 		tracker = ga.getAll()[0];
-		if (tracker) {		
-			//Top Navs
+		if (tracker) {	
+			console.log("tracker found");
 			$(".tixnav").on("click", function(event){
 				event.stopPropogation();
 				event.stopImmediatePropogation();
 				var evLabel = $(this).attr("id");
+				console.log(evLabel);
 				tracker.send("event", "nav", "click", evLabel);
 			});
 		}
