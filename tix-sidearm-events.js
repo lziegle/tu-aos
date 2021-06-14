e7191079dff99d99256a5b3d1f5b3e13b093688b
lziegle <71145305+lziegle@users.noreply.github.com>
2021-06-14 09:59:47 -0400

    Update tix-sidearm-events.js

--- tix-sidearm-events.js
+++ tix-sidearm-events.js
@@ -1,74 +1,10 @@
 $(document).ready(function(){
-	var lz_test = "added";
 	var tracker = '';
 	if ("ga" in window) {
 		ga(function() {
 			window.tracker = ga.getAll()[0];
 		});
 		if (window.tracker) {	
-			$("#fbGroupDeposit").click(function(){
-				var sourceValue = 'fb.group';
-				var contentValue = 'btn.mid';
-			
-				window.tracker.set('dimension2', sourceValue);
-				window.tracker.set('dimension4', contentValue);
-				window.tracker.send("event", "buy", "click", "fbGroupDeposit");
-			});
-			
-			$("#fbAwayCincyBuy").click(function(){
-				var sourceValue = 'fb.away';
-				var contentValue = 'btn.card';
-			
-				window.tracker.set('dimension2', sourceValue);
-				window.tracker.set('dimension4', contentValue);
-				window.tracker.send("event", "buy", "click", "fbAwayCincy");
-			});
-			
-			$("#fbAwayUSFBuy").click(function(){
-				var sourceValue = 'fb.away';
-				var contentValue = 'btn.card';
-			
-				window.tracker.set('dimension2', sourceValue);
-				window.tracker.set('dimension4', contentValue);
-				window.tracker.send("event", "buy", "click", "fbAwayUSF");
-			});
-			
-			$("#fbAwayECUBuy").click(function(){
-				var sourceValue = 'fb.away';
-				var contentValue = 'btn.card';
-			
-				window.tracker.set('dimension2', sourceValue);
-				window.tracker.set('dimension4', contentValue);
-				window.tracker.send("event", "buy", "click", "fbAwayECU");
-			});
-			
-			$("#fbAwayTulsaBuy").click(function(){
-				var sourceValue = 'fb.away';
-				var contentValue = 'btn.card';
-			
-				window.tracker.set('dimension2', sourceValue);
-				window.tracker.set('dimension4', contentValue);
-				window.tracker.send("event", "buy", "click", "fbAwayTulsa");
-			});
-			
-			$("#fbAwayRutgersBuy").click(function(){
-				var sourceValue = 'fb.away';
-				var contentValue = 'btn.card';
-			
-				window.tracker.set('dimension2', sourceValue);
-				window.tracker.set('dimension4', contentValue);
-				window.tracker.send("event", "buy", "click", "fbAwayRutgers");
-			});
-			
-			$("#fbAwayAkronBuy").click(function(){
-				var sourceValue = 'fb.away';
-				var contentValue = 'btn.card';
-			
-				window.tracker.set('dimension2', sourceValue);
-				window.tracker.set('dimension4', contentValue);
-				window.tracker.send("event", "buy", "click", "fbAwayAkron");
-			});
-			
 			$(".tix-itm").click(function(event){
 				var evLabel = $(this).attr("id");
 				var sourceValue = $(this).attr("data-source");
@@ -83,7 +19,6 @@ $(document).ready(function(){
 				// event.stopPropogation();
 				// event.stopImmediatePropogation();
 				var evLabel = $(this).attr("id");
-				console.log(evLabel);
 				window.tracker.send("event", "nav", "click", evLabel);
 			});
 		}
