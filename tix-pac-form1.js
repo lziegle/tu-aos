784f031a851a0a9f30bb59537a31219111d488d0
lziegle <71145305+lziegle@users.noreply.github.com>
2021-06-29 17:36:07 -0400

    Update tix-pac-form1.js

--- tix-pac-form1.js
+++ tix-pac-form1.js
@@ -5,4 +5,16 @@ link.rel = 'shortcut icon';
 link.href = 'https://am-prod-client-files.ppub-tmaws.io/temple/s3fs-public/favicon_temple.ico';
 document.getElementsByTagName('head')[0].appendChild(link);
 
+//hide each line
+var SHide = $('[name^="renewSH"]');
+for (x in sHide) {
+  $(sHide[x]).hide();
+}
 
+//fill in value from main question to each indiv line
+function fillSH() {
+  var SHinput = this.value;
+  var SHlist = $('[name^="renewSH"]');
+  for (i in SHlist) {
+    SHlist[i].value = SHinput;
+}
