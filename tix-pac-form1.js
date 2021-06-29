ac4dfe448b4d7c0a8b6badd549975b19ac086955
lziegle <71145305+lziegle@users.noreply.github.com>
2021-06-29 16:49:00 -0400

    Update tix-pac-form1.js

--- tix-pac-form1.js
+++ tix-pac-form1.js
@@ -1,1 +1,8 @@
+//Add Temple favicon
+var link = document.createElement('link');
+link.type = 'image/x-icon';
+link.rel = 'shortcut icon';
+link.href = 'https://am-prod-client-files.ppub-tmaws.io/temple/s3fs-public/favicon_temple.ico';
+document.getElementsByTagName('head')[0].appendChild(link);
+
 
