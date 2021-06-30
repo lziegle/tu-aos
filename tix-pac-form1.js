0e698c40f48ba176ca3959e12d0acb1b4bac6c24
lziegle <71145305+lziegle@users.noreply.github.com>
2021-06-30 11:13:01 -0400

    Update tix-pac-form1.js

--- tix-pac-form1.js
+++ tix-pac-form1.js
@@ -1,21 +1,23 @@
-//Add Temple favicon
-var link = document.createElement('link');
-link.type = 'image/x-icon';
-link.rel = 'shortcut icon';
-link.href = 'https://am-prod-client-files.ppub-tmaws.io/temple/s3fs-public/favicon_temple.ico';
-document.getElementsByTagName('head')[0].appendChild(link);
+$(document).ready(function(){
+  //Add Temple favicon
+  var link = document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = 'https://am-prod-client-files.ppub-tmaws.io/temple/s3fs-public/favicon_temple.ico';
+  document.getElementsByTagName('head')[0].appendChild(link);
 
-//hide each input
-$('td[id^="specreqCell_"]').each(function(){$(this).css("visibility","hidden")});
-
-
-//create main input boxes
+  //hide each input
+  $('td[id^="specreqCell_"]').each(function(){$(this).css("visibility","hidden")});
 
 
-//fill in value from main question to each indiv line
-function fillSH() {
-  var SHinput = this.value;
-  var SHlist = $('[name^="renewSH"]');
-  for (i in SHlist) {
-    SHlist[i].value = SHinput;
-}
+  //create main input boxes
+
+
+  //fill in value from main question to each indiv line
+  function fillSH() {
+    var SHinput = this.value;
+    var SHlist = $('[name^="renewSH"]');
+    for (i in SHlist) {
+      SHlist[i].value = SHinput;
+  }
+  });
