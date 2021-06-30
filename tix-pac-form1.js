b1bbcd9aca9dd785dd36d5e1a4bf77089f2d0d73
lziegle <71145305+lziegle@users.noreply.github.com>
2021-06-30 12:15:35 -0400

    Update tix-pac-form1.js

--- tix-pac-form1.js
+++ tix-pac-form1.js
@@ -11,14 +11,15 @@ $(document).ready(function(){
 
 
   //create main input boxes
-
+  $('.ev_PayPlan').after('<tr><td colspan="8" style="padding-bottom: 1em;"><b>Would you like to enroll in an auto 12-Month Interest-Free Payment Plan starting for the 2022-23 Basketball Season?</b><select name="12monthauto" id="12monthauto"><option value="YES">YES</option><option value="NO" selected="">NO</option></select></td></tr>')
 
   //fill in value from main question to each indiv line
-  function fillSH() {
-    var SHinput = this.value;
+  $("#12monthauto").on('change', function(){
+	var SHinput = this.value;
     var SHlist = $('[name^="renewSH"]');
     for (i in SHlist) {
-      SHlist[i].value = SHinput;
-      }
-  }
+      SHlist[i].value = "12mth: "+SHinput;
+      }});
   });
+
+
