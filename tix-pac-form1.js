a67d89ba907921354f19211cc4b283681f5d7974
lziegle <71145305+lziegle@users.noreply.github.com>
2021-07-15 16:06:02 -0400

    Update tix-pac-form1.js

--- tix-pac-form1.js
+++ tix-pac-form1.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
   //create main input boxes
   $('.ev_PayPlan').after('<tr><td colspan="8" style="padding-bottom: 1em;"><b>Would you like to enroll in a 12-Month Interest-Free Payment Plan starting for the 2022-23 Basketball Season?</b><select name="12monthauto" id="12monthauto"><option value="YES">YES</option><option value="NO" selected="">NO</option></select></td></tr>')
-  $('.ev_PayPlan').after('<tr><td colspan="8" style="padding-bottom: 1em;"><b>Are you interested in upgrading your seat location for the 2021-22 Season?</b><select name="2122upg" id="2122upg"><option value="YES">YES</option><option value="NO" selected="">NO</option></select></td></tr>')
+  $('.ev_PayPlan').after('<tr><td colspan="8" style="padding-bottom: 1em;"><b>Are you interested in upgrading your seat location for the 2021-22 Season?</b><select name="2122upg" id="2122upg"><option value="YES">YES</option><option value="NO" selected="">NO</option></select><br><span style="font-size:10px;font-weight:normal">(Automatic payments run from November 2021 through October 2022. If prices change for the 2022-23 season, you will be notified before the amount of your monthly payment changes.)</span></td></tr>')
 	
 	
   //fill in value from main question to each indiv line
