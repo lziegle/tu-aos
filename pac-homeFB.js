b7f43b542acaec00a518b15cd2e489884e9cc916
lziegle <71145305+lziegle@users.noreply.github.com>
2021-08-29 19:27:01 -0400

    Update pac-homeFB.js

--- pac-homeFB.js
+++ pac-homeFB.js
@@ -6,7 +6,7 @@
   link.href = 'https://am-prod-client-files.ppub-tmaws.io/temple/s3fs-public/favicon_temple.ico';
   document.getElementsByTagName('head')[0].appendChild(link);
   
-  var games = getElementsByClassName('cust-HomeGame');
+  var games = document.getElementsByClassName('cust-HomeGame');
   for (item in games) {
     games[item].classList.add('btn', 'btn-primary', 'btn-sm', 'btn-block');
   }
