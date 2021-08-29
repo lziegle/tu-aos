395f192d5ca32e0f874ef4712bffd8387297741d
lziegle <71145305+lziegle@users.noreply.github.com>
2021-08-29 18:46:37 -0400

    Create pac-homeFB.js

---
+++ pac-homeFB.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  //Add Temple favicon
+  var link = document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = 'https://am-prod-client-files.ppub-tmaws.io/temple/s3fs-public/favicon_temple.ico';
+  document.getElementsByTagName('head')[0].appendChild(link);
+  
+  $(".cust-HomeGames").each(function() {
+    $(this).addClass("btn btn-primary btn-sm btn-block");
+  });
+  
+});
